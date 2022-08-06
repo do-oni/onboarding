81ad7fcc52a98120985b1e61729b68b935eecd8b
do-oni <dooniseo@gmail.com>
2022-08-06 15:39:32 +0900

    Add: introduction screen page

--- lib/onboarding_page.dart
+++ lib/onboarding_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({Key? key}) : super(key: key);
@@ -7,7 +8,23 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Hello onboardingPage")),
+      body: IntroductionScreen(
+        pages: [
+          // 페이지 1
+          PageViewModel(
+            title: "Title1",
+            bodyWidget: Text("body1"),
+          ),
+          // 페이지 2
+          PageViewModel(
+            title: "Title2",
+            bodyWidget: Text("body2"),
+          ),
+        ],
+        next: Text("Next", style: TextStyle(fontWeight: FontWeight.w600)),
+        done: Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+        onDone: () {},
+      ),
     );
   }
 }
