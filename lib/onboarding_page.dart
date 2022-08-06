1f5bfa8bf4d266c31f8cc22cc624e507ef850371
do-oni <dooniseo@gmail.com>
2022-08-06 15:33:35 +0900

    Add: onboarding page

---
+++ lib/onboarding_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Hello onboardingPage")),
+    );
+  }
+}
